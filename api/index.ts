942eb4fa6dc85cf227bbb502c4e1787479601776
s4malve <s4malve@gmail.com>
2023-01-18 17:07:12 -0500

    feat: add animes endpoint with page and per_page queries

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,8 @@
 import { Hono } from 'hono'
 
+import animes from '../db/animes.json'
+import { paginator } from './utils'
+
 const app = new Hono()
 
 app.get('/', (c) =>
@@ -7,5 +10,13 @@ app.get('/', (c) =>
     welcome: 'Hello World'
   })
 )
+
+app.get('/animes', (ctx) => {
+  const query = ctx.req.query()
+  const page = Number(query?.page) ?? 1
+  const per_page = Number(query?.per_page) ?? 10
+
+  return ctx.json(paginator(animes, page, per_page))
+})
 
 export default app
