8b889707dd91e763eff1729291b0a32fb8d673ca
s4malve <s4malve@gmail.com>
2023-01-23 17:51:29 -0500

    feat: add transition to transform in BUTTON

--- src/css/styles.ts
+++ src/css/styles.ts
@@ -1,4 +1,4 @@
 export const BUTTON = {
   primary:
-    'bg-stone-100 bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded border border-transparent focus-within:border-stone-100/40 hover:border-stone-100/40 active:border-stone-100/40 transition-colors'
+    'bg-stone-100 bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded border border-transparent focus-within:border-stone-100/40 hover:border-stone-100/40 active:border-stone-100/40 transition-[color,transform]'
 }
