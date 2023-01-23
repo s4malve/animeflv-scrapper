8eeb3d1f3a3054aa1fb1a0048f3f756e0b98652c
s4malve <s4malve@gmail.com>
2023-01-23 18:49:41 -0500

    fix: remove max width from WRAPPER

--- src/css/styles.ts
+++ src/css/styles.ts
@@ -3,4 +3,4 @@ export const BUTTON = {
     'bg-stone-100 bg-opacity-20 backdrop-blur-lg drop-shadow-lg rounded border border-transparent focus-within:border-stone-100/40 hover:border-stone-100/40 active:border-stone-100/40 transition-[color,transform]'
 }
 
-export const WRAPPER = 'w-11/12 mx-auto max-w-7xl'
+export const WRAPPER = 'w-11/12 mx-auto'
