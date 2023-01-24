bcf67488243d0348ab9c14e19348ed02abd4f888
s4malve <s4malve@gmail.com>
2023-01-24 17:34:21 -0500

    feat: put extracted function from utils

--- scrapper/all-animes.js
+++ scrapper/all-animes.js
@@ -1,17 +1,68 @@
-import { resolve } from 'node:path'
+import { scrapper, URLS, writeDbFile } from './utils'
 
-import { getAllAnimesInPage, writeFile } from './utils'
+export const getAllAnimes = async () => {
+  const animes = []
+  const MAX_ANIME_INDEX = 1
+  const getAnimesFromPage = async (pageNumber) => {
+    const url = `${URLS.animeflv.BASE}/${URLS.animeflv.ALL_ANIMES}?page=${pageNumber}`
+    const selector = 'ul.ListAnimes.AX.Rows.A03.C02.D02 li'
+    const animesFromPage = await scrapper({
+      selectors: {
+        id: {
+          selector: 'article.Anime.alt.B a',
+          action: {
+            name: 'attr',
+            value: 'href'
+          },
+          format: (text) => text.slice(7)
+        },
+        title: {
+          selector: 'h3.Title',
+          action: {
+            name: 'text',
+            value: undefined
+          },
+          format: null
+        },
+        descripton: {
+          selector: 'div.Description p + p',
+          action: {
+            name: 'text',
+            value: undefined
+          },
+          format: null
+        },
+        image: {
+          selector: 'div.Image.fa-play-circle-o img',
+          action: {
+            name: 'attr',
+            value: 'src'
+          },
+          format: null
+        },
+        type: {
+          selector: 'div.Image.fa-play-circle-o span.Type',
+          action: {
+            name: 'text',
+            value: undefined
+          },
+          format: null
+        }
+      },
+      url,
+      selector
+    })
 
-const animes = []
+    return animesFromPage
+  }
 
-const MAX_ANIME_INDEX = 1
+  for (let i = 1; i <= MAX_ANIME_INDEX; i++) {
+    const animesInPage = await getAnimesFromPage(i)
 
-for (let i = 1; i <= MAX_ANIME_INDEX; i++) {
-  const animesInPage = await getAllAnimesInPage(i)
-
-  animes.push(...animesInPage)
+    animes.push(...animesInPage)
+  }
 }
 
-const filePath = resolve('./db/animes.json')
+const animes = await getAllAnimes()
 
-await writeFile(filePath, animes)
+await writeDbFile('animes', animes)
--- scrapper/todays-animes.js
+++ scrapper/todays-animes.js
@@ -1,30 +1,92 @@
 import {
-  getTodaysAnimes,
-  writeFile,
-  downloadAsset,
   URLS,
   emptyDir,
-  isDirEmpty
+  isDirEmpty,
+  getProvidersLink,
+  PROVIDERS_NAME,
+  getDownloadLinkFromMainProvider,
+  getDownloadLinkFromAlternativeProvider,
+  scrapper,
+  writeDbFile,
+  downloadAsset
 } from './utils.js'
 import { resolve } from 'node:path'
 
-const rawTodaysAnimes = await getTodaysAnimes()
-
 const ASSETS_PATH = resolve(URLS.api.lastEpidodesThumbnails)
 
-if (!(await isDirEmpty(URLS.api.lastEpidodesThumbnails))) {
-  await emptyDir(URLS.api.lastEpidodesThumbnails)
+export const getTodaysAnimes = async () => {
+  const rawTodaysAnimes = await scrapper({
+    selectors: {
+      id: {
+        selector: 'a',
+        action: {
+          name: 'attr',
+          value: 'href'
+        },
+        format: (text) => text.slice(5)
+      },
+      thumbnail: {
+        selector: 'span.Image img',
+        action: {
+          name: 'attr',
+          value: 'src'
+        },
+        format: (text) => `${URLS.animeflv.BASE}${text}`
+      },
+      episode: {
+        selector: 'span.Capi',
+        action: {
+          name: 'text',
+          value: undefined
+        },
+        format: (text) => Number(text.split(' ')[1])
+      },
+      anime: {
+        selector: 'strong.Title',
+        action: {
+          name: 'text',
+          value: undefined
+        },
+        format: null
+      }
+    },
+    url: URLS.animeflv.BASE,
+    selector: 'ul.ListEpisodios.AX.Rows.A06.C04.D03 li'
+  })
+
+  if (!(await isDirEmpty(ASSETS_PATH))) {
+    await emptyDir(ASSETS_PATH)
+  }
+
+  const todaysAnimes = await Promise.all(
+    rawTodaysAnimes.map(async (anime) => {
+      const providers = await getProvidersLink(anime.id)
+      const provider = providers.find(
+        ({ name, path }) =>
+          name === PROVIDERS_NAME.MAIN || name === PROVIDERS_NAME.ALTERNATIVE
+      )
+      const downloadLink =
+        provider.name === PROVIDERS_NAME.MAIN
+          ? await getDownloadLinkFromMainProvider(provider.path)
+          : await getDownloadLinkFromAlternativeProvider(provider.path)
+
+      await downloadAsset({
+        pathFrom: anime.thumbnail,
+        pathTo: ASSETS_PATH,
+        fileName: `${anime.id}.jpg`
+      })
+
+      return {
+        ...anime,
+        thumbnail: `${URLS.base}/static/thumbnails/last-episodes/${anime.id}.jpg`,
+        downloadLink
+      }
+    })
+  )
+
+  return todaysAnimes
 }
 
-rawTodaysAnimes.forEach(async ({ thumbnail, id }) => {
-  await downloadAsset(thumbnail, `${ASSETS_PATH}/${id}.jpg`)
-})
+const todaysAnimes = await getTodaysAnimes()
 
-const todaysAnimes = rawTodaysAnimes.map((anime) => ({
-  ...anime,
-  thumbnail: `${URLS.base}/static/thumbnails/${anime.id}.jpg`
-}))
-
-const FILE_PATH = resolve('./db/todays-animes.json')
-
-await writeFile(FILE_PATH, todaysAnimes)
+await writeDbFile('todays-animes', todaysAnimes)
