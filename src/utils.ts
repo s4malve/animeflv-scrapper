b4437bbef44759dd46ea4fd19ecf370364934b96
s4malve <s4malve@gmail.com>
2023-01-26 18:22:05 -0500

    fix: logic of getBgImgDependsOnTime

--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,27 @@
 export const getBgImgDependsOnTime = (hour: number) => {
-  const times = {
-    morning: { min: 6, max: 11 },
-    afternoon: { min: 12, max: 18 },
-    night: { min: 19, max: 5 }
-  }
-  const timesEntries = Object.entries(times)
-  const currentTime = timesEntries.find(
-    ([_, { max, min }]) => hour >= min && hour <= max
+  const times = [
+    {
+      name: 'night',
+      minMax: [0, 4]
+    },
+    {
+      name: 'morning',
+      minMax: [5, 11]
+    },
+    {
+      name: 'afternoon',
+      minMax: [12, 18]
+    },
+    {
+      name: 'night',
+      minMax: [19, 24]
+    }
+  ]
+
+  const currentTime = times.find(
+    ({ minMax: [min, max] }) => hour >= min && hour <= max
   )
-  const [timeName] = currentTime ?? ['']
+  const timeName = currentTime?.name
 
   return `${timeName}-cover.jpg`
 }
